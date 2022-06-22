14f583b4c00f229d119446037fd21b5c740fda22
azerty0301 <morozumiyumie@gmail.com>
2022-06-22 14:23:58 +0900

    2022/06/22/jsLesson1~6,catapp,Multiplication table,studyapp追加

---
+++ fruitsapp/js/main.js
@@ -0,0 +1,23 @@
+window.onload=()=>{
+    //const prices=[120,50,180];
+    const result=document.getElementById("result");
+    const fruits=document.getElementsByClassName("fruits");
+    for(let f of fruits){
+        f.addEventListener("change", numberChange);
+    }
+    function numberChange(){
+        let sum=0;
+        /*
+        for(let i=0;i<fruits.length;i++){
+            sum += fruits[i].value * fruits[i].getAttribute("data-price");
+        }
+        */
+       for(let f of fruits){
+        sum += f.value * f.getAttribute("data-price");
+       };
+        result.textContent=sum + "円です!";
+    };
+    for(let f of fruits){
+        f.addEventListener("change",numberChange)
+    }
+}
